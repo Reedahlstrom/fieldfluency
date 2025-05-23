2db35871f2313c9ab69fc2313b07045b28d0cd9f
Reedahlstrom <reedahlstrom@gmail.com>
2025-05-23 15:17:52 -0600

    Update gpt_feedback.py

--- gpt_feedback.py
+++ gpt_feedback.py
@@ -6,7 +6,7 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def get_feedback(prompt):
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo"
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content
